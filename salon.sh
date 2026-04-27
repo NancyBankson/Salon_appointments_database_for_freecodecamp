058bb3df9b5c1dcec408b6727da455496f39b623
Nancy Bankson <nbankson3@gmail.com>
2026-04-27 11:10:35 -0500

    Update salon.sh

--- salon.sh
+++ salon.sh
@@ -30,23 +30,23 @@ done
 SERVICE=$($PSQL "SELECT name FROM services WHERE service_id='$SERVICE_ID_SELECTED'")
 # get phone number
 echo -e "\nWhat's your phone number?"
-read PHONE_NUMBER
+read CUSTOMER_PHONE
 # get customer_id
-IFS="|" read CUSTOMER_ID NAME <<< $($PSQL "SELECT customer_id, name FROM customers WHERE phone='$PHONE_NUMBER'")
+IFS="|" read CUSTOMER_ID CUSTOMER_NAME <<< $($PSQL "SELECT customer_id, name FROM customers WHERE phone='$CUSTOMER_PHONE'")
 # if not found
 if [[ -z $CUSTOMER_ID ]]
 then
   echo -e "\nI don't have a record for that phone number, what's your name?"
   # get name
-  read NAME
-  NAME_RESULT=$($PSQL "INSERT INTO customers (phone, name) VALUES ('$PHONE_NUMBER', '$NAME')")
+  read CUSTOMER_NAME
+  NAME_RESULT=$($PSQL "INSERT INTO customers (phone, name) VALUES ('$CUSTOMER_PHONE', '$CUSTOMER_NAME')")
   if [[ $NAME_RESULT == "INSERT 0 1" ]]
   then
-    CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$PHONE_NUMBER'")
+    CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
   fi
 fi
 # get time
-echo -e "\nWhat time would you like your $(echo $SERVICE | sed -r 's/^ *| *$//g'), $(echo $NAME | sed -r 's/^ *| *$//g')?" 
-read TIME
-TIME_RESULT=$($PSQL "INSERT INTO appointments (customer_id, service_id, time) VALUES ('$CUSTOMER_ID','$SERVICE_ID_SELECTED','$TIME')")
-echo -e "\nI have put you down for a $(echo $SERVICE | sed -r 's/^ *| *$//g') at $(echo $TIME | sed -r 's/^ *| *$//g'), $(echo $NAME | sed -r 's/^ *| *$//g')."
+echo -e "\nWhat time would you like your $(echo $SERVICE | sed -r 's/^ *| *$//g'), $(echo $CUSTOMER_NAME | sed -r 's/^ *| *$//g')?" 
+read SERVICE_TIME
+TIME_RESULT=$($PSQL "INSERT INTO appointments (customer_id, service_id, time) VALUES ('$CUSTOMER_ID','$SERVICE_ID_SELECTED','$SERVICE_TIME')")
+echo -e "\nI have put you down for a $(echo $SERVICE | sed -r 's/^ *| *$//g') at $(echo $SERVICE_TIME | sed -r 's/^ *| *$//g'), $(echo $CUSTOMER_NAME | sed -r 's/^ *| *$//g')."
