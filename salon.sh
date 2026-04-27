18f50aec4e536a603bf25afb6377dd0aef9dc94d
Nancy Bankson <nbankson3@gmail.com>
2026-04-27 10:56:32 -0500

    Update salon.sh

--- salon.sh
+++ salon.sh
@@ -4,12 +4,49 @@ AVAILABLE_SERVICES=$($PSQL "SELECT service_id, name FROM services ORDER BY servi
 
 echo -e "\n~~~~~ MY SALON ~~~~~"
 
-MAIN_MENU() {
-  echo -e "\nWelcome to My Salon, how can I help you?\n"
-  echo "$AVAILABLE_SERVICES" | while read SERVICE_ID BAR SERVICE_NAME
+GET_SERVICES() {
+ echo "$AVAILABLE_SERVICES" | while read SERVICE_ID BAR SERVICE_NAME
     do
       echo "$SERVICE_ID) $SERVICE_NAME"
     done
 }
 
-MAIN_MENU
+echo -e "\nWelcome to My Salon, how can I help you?\n"
+while true
+do
+  # Display services
+  GET_SERVICES
+
+  # Get user input
+  read SERVICE_ID_SELECTED
+
+  # Check if input is a valid number (1-5)
+  if [[ $SERVICE_ID_SELECTED =~ ^[1-5]$ ]]; then
+    break
+  else
+    echo -e "\nI could not find that service. What would you like today?"
+  fi
+done
+SERVICE=$($PSQL "SELECT name FROM services WHERE service_id='$SERVICE_ID_SELECTED'")
+# get phone number
+echo -e "\nWhat's your phone number?"
+read PHONE_NUMBER
+# get customer_id
+IFS="|" read CUSTOMER_ID NAME <<< $($PSQL "SELECT customer_id, name FROM customers WHERE phone='$PHONE_NUMBER'")
+# if not found
+if [[ -z $CUSTOMER_ID ]]
+then
+  echo -e "\nI don't have a record for that phone number, what's your name?"
+  # get name
+  read NAME
+  NAME_RESULT=$($PSQL "INSERT INTO customers (phone, name) VALUES ('$PHONE_NUMBER', '$NAME')")
+  if [[ $NAME_RESULT == "INSERT 0 1" ]]
+  then
+    CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$PHONE_NUMBER'")
+  fi
+fi
+# get time
+echo -e "\nWhat time would you like your $SERVICE, $NAME?" 
+read TIME
+TIME_RESULT=$($PSQL "INSERT INTO appointments (customer_id, service_id, time) VALUES ('$CUSTOMER_ID','$SERVICE_ID_SELECTED','$TIME')")
+echo -e "\nI have put you down for a $SERVICE at $TIME, $NAME."
